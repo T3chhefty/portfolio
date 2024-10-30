1d89524dab325ab6d38a62d479594ed8747591fb
T3chhefty <shadrachndu85@gmail.com>
2024-10-30 14:01:40 +0100

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero(props) {
         <div className='hero-container'>
             <div className='hero-opacity'>
                 <h2 className='id-me'>My Name is Shadrach Ndu <br />
-                    I am a web <span className='orange'>Developer</span> </h2>
+                    I am a Frontend web <span className='orange'>Developer</span> </h2>
                 {/* <p style={{fontSize:"16px", color:"orange"}}>Download my Cv to know more about me.</p> */}
                 <Button text='Download CV'/>
             </div>
